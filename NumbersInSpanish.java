24dd67bb0ac168ed6054f1a00241f113b65a0713
jhf214 <jhf214@exeter.ac.uk>
2026-01-30 11:59:08 +0000

    NumbersInSpanish completed with switch-case

--- NumbersInSpanish.java
+++ NumbersInSpanish.java
@@ -3,18 +3,23 @@ public class NumbersInSpanish {
        // read the first argument passed to the program
        int number = Integer.parseInt(args[0]); 
  
-       if (number == 1) {
+       switch(number) {
+        case 1:
             System.out.println("uno");
-        } else if ( number == 2 ) {
+            break;
+        case 2:
             System.out.println("dos");
-        } else if ( number == 3 ) {
+            break;
+        case 3:
             System.out.println("tres");
-        } else if ( number == 4 ) {
+            break;
+        case 4:
             System.out.println("cuatro");
-        } else if ( number == 5 ) {
+            break;
+        case 5:
             System.out.println("cinqo");
-
-        } else {
+            break;
+        default:
             System.out.println("que quosaaa");
         }
     }
